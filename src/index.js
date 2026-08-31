fe0c5ccae952e07354dc10d56e69ca3dfcf01cb9
Ranna20 <rannadignity@gmail.com>
2026-08-31 05:40:51 +0200

    updated the temperature data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+//change the temperature and city name in the UI
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+    let cityElement = document.querySelector("#cityName");
+    cityElement.innerHTML = response.data.city;   
+}
+//make api call and update the UI only search for a city
+function searchCity(city) {
+  let apikey = "c9o83a4fd3bf05dt29b93022d8402160";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 //change city name when user submits the form
-function handleSearchFormSubmit(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-  let cityName = document.querySelector("#cityName");
-  cityName.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchFormSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+//search for a default city when the page loads
+searchCity("Paris");
